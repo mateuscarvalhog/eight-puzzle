792af428274143ea39bfc74bc94ae246b8c6bd2a
Pedro <pedro.souza2@estudante.ufla.br>
2022-02-27 14:02:46 -0300

    type of objective_state had been fixed

--- puzzle.py
+++ puzzle.py
@@ -1,18 +1,21 @@
+from position import Position
 from puzzle_state import PuzzleState
 from agent import Agent
 
 class Puzzle:
-
     def __init__(self):
         self.initial_state = self.generate_initial_state()
         self.solution = self.solve()
 
+        print('\n'.join(map(lambda state: state.to_string(), self.solution)))
+
     def generate_initial_state(self):
-        return PuzzleState([[1, 2, 3], [4, 5, 6], [7, None, 8]])  # TODO mudar constante de estado inicial
+        return PuzzleState([[7, 3, 5], [2, 8, 6], [4, 1, None]])  # TODO mudar constante de estado inicial
     
     def solve(self):
-        solver = Agent(self.initial_state, [[1, 2, 3], [4, 5, 6], [7, 8, None]]) # TODO mudar constante de estado objetivo
-        return []
+        objective_state = PuzzleState([[1, 2, 3], [4, 5, 6], [7, 8, None]])
+        solver = Agent(self.initial_state, objective_state) # TODO mudar constante de estado objetivo
+        return solver.play_with_blind_search()
 
 
 def main():
