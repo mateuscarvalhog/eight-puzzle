f5e6e1007570cdffc2084ad1bd697ff72044e44c
Pedro <pedro.souza2@estudante.ufla.br>
2022-02-28 19:40:51 -0300

    class menu had been implemented

---
+++ menu.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Menu:
+    def __init__(self, options: List[tuple], title: str = None):
+        self.options = options
+        self.title = title
+
+    def show(self, show_title = False):
+        if show_title and self.title is not None:
+            print('\n' + '-'*20 + '\n' + self.title + '\n')
+        
+        for option in self.options:
+            number, text = option
+            print(f'{number}. {text}')
+
+    def wait_an_option(self, show_menu = False, show_title = False):
+        if show_menu:
+            self.show(show_title)
+        
+        chosen_option = int(input("\nChoose an option: "))
+
+        if self.option_is_valid(chosen_option):
+            return chosen_option
+        else:
+            print('\nChoose a valid option!\n')
+            self.show()
+            self.wait_an_option()
+
+    def option_is_valid(self, option):
+        return any(valid_option == option for valid_option, _ in self.options)
