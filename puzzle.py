acd79e2b7d49b29128f116ef637846db996198ba
Pedro <pedro.souza2@estudante.ufla.br>
2022-03-01 14:58:33 -0300

    set initial state had been fixed

--- puzzle.py
+++ puzzle.py
@@ -7,12 +7,12 @@ import time
 import os
 
 class Puzzle:
-    def __init__(self):
+    def __init__(self, initial_state_str: str):
         self.objective_state = PuzzleState([[1, 2, 3], [4, 5, 6], [7, 8, None]])
-        self.initial_state = self.generate_initial_state()
+        self.initial_state = self.generate_random_initial_state() if initial_state_str is None else self.build_initial_state_from_str(initial_state_str)
         self.solver = Agent(self.initial_state, self.objective_state)
 
-    def generate_initial_state(self):
+    def generate_random_initial_state(self):
         number_of_iterations = randint(500, 1500)
         initial_state = self.objective_state
 
@@ -23,7 +23,7 @@ class Puzzle:
 
         return initial_state
     
-    def set_initial_state(self, initial_state_str: str):
+    def build_initial_state_from_str(self, initial_state_str: str):
         matrix_of_initial_state = list()
 
         for index in range(len(initial_state_str)):
@@ -36,7 +36,7 @@ class Puzzle:
 
             matrix_of_initial_state[row].append(element)
 
-        self.initial_state = PuzzleState(matrix_of_initial_state)
+        return PuzzleState(matrix_of_initial_state)
 
     def solve_with_blind_search(self):
         print("\n\nSolving puzzle with Breadth-First blind search method...")
@@ -63,11 +63,13 @@ class Puzzle:
         print(search_result)
 
     def print_solving_steps(self, trace_route: List[PuzzleState]):
+        MAX_DURATION_IN_SEC = 1.5
+        interval_in_sec = MAX_DURATION_IN_SEC/len(trace_route)
         clear_console = lambda: os.system('cls' if os.name=='nt' else 'clear')
         for state in trace_route:
             clear_console()
             print(state)
-            time.sleep(.001)
+            time.sleep(interval_in_sec)
 
 def main():
     search_menu = Menu([(1, 'Blind search (Breadth-first)'), (2, 'Heuristic search (Manhattan distance)')], 'Choose a search method')
@@ -78,11 +80,11 @@ def main():
     game_option = main_menu.wait_an_option()
 
     while game_option != 0:
-        puzzle = Puzzle()
-
+        initial_state = None
         if game_option == 2:
-            initial_state = input("Enter game state, from top-left to right-bottom, 9 characters, e.g. \"1034526478\": ")
-            puzzle.set_initial_state(initial_state)
+            initial_state = input("Enter game state, from top-left to right-bottom, 9 characters, e.g. \"413576208\": ")
+
+        puzzle = Puzzle(initial_state)
         
         print("\nYour initial state:")
         print(puzzle.initial_state)
